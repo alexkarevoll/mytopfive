bacd99c145171749ef97ae62b9feaf2578541b98
alexkarevoll <alex.karevoll@gmail.com>
2017-02-09 19:06:57 -0800

    basic routes and models

---
+++ server/models/Album.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Album = new Schema({
+  title: String,
+})
+
+module.exports = mongoose.model('Album', Album)
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
 
+const Album = require('../models/Album.js')
 
+router.get('/albums', function(req, res) {
+  Album.find({}, function(err, albums) {
+    if(err) return console.log(err)
+    res.json(albums)
+  })
+})
+
+router.get('/albums/:id', function(req,res) {
+  Album.findById(req.params.id, function(err,album) {
+    if(err) return console.log(err)
+    res.json(album)
+  })
+})
+
+router.post('/albums', function(req,res) {
+  Album.create(req.body, function(err, album) {
+    if(err) return console.log(err)
+    res.json({success: true, message: 'album created', album: album})
+  })
+})
 
 module.exports = router;
