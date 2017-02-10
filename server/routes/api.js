ecde7189799fba6b1beffdefba248f1f41436937
alexkarevoll <alex.karevoll@gmail.com>
2017-02-09 19:22:53 -0800

    added update destroy and seed routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose')
+const seed = require('../seed.js')
 
 const Album = require('../models/Album.js')
 
+// INDEX
 router.get('/albums', function(req, res) {
   Album.find({}, function(err, albums) {
     if(err) return console.log(err)
@@ -11,6 +13,15 @@ router.get('/albums', function(req, res) {
   })
 })
 
+// POST
+router.post('/albums', function(req,res) {
+  Album.create(req.body, function(err, album) {
+    if(err) return console.log(err)
+    res.json({success: true, message: 'album created', album: album})
+  })
+})
+
+// SHOW
 router.get('/albums/:id', function(req,res) {
   Album.findById(req.params.id, function(err,album) {
     if(err) return console.log(err)
@@ -18,10 +29,30 @@ router.get('/albums/:id', function(req,res) {
   })
 })
 
-router.post('/albums', function(req,res) {
-  Album.create(req.body, function(err, album) {
+// UPDATE
+router.patch('/albums/:id', function(req,res) {
+  Album.findByIdAndUpdate(req.params.id, req.body, {new:true}, function(err, album) {
     if(err) return console.log(err)
-    res.json({success: true, message: 'album created', album: album})
+    res.json({success: true, message: "album updated", album: album})
+  })
+})
+
+// DESTORY
+router.delete('/albums/:id', function(req, res) {
+  Album.findByIdAndRemove(req.params,id, function(err) {
+    if(err) return console.log(err)
+    res.json({success: true, message: "album deleted"})
+  })
+})
+
+// SEED DB
+router.get('/seed', function(req,res) {
+  Album.remove({}, function(err) {
+    if(err) return console.log(err)
+    Album.insertMany(seed, function(err, albums) {
+      if(err) return console.log(err)
+      res.json({success: true, message: "database seeded", albums:albums})
+    })
   })
 })
 
---
+++ server/seed.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    title: "The Low End Theory"
+  },
+  {
+    title: "Discovery"
+  },
+  {
+    title: "Stop Making Sense"
+  },
+  {
+    title: "The Warning"
+  },
+  {
+    title: "Dookie"
+  },
+]
